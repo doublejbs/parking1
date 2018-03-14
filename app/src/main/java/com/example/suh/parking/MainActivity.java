3accacd83b1a63e2453a8d240bfc95e01877116c
psh <ray5273@naver.com>
2018-03-14 15:42:41 +0900

    Commit : 1 loading 화면을 추가했는데 activity는 추가하지 않았고 mainactivity를 켜면서 잠깐 사진을 보여주고 다시 원래 거로 바꿔주는 방식이에요
    그래서 나중에 사진 수정할때는 res/drawable/background_splash.xml 파일만 수정하시면 될거에요.
    궁금한거 있으면 나중에 다시 설명 해 드릴게요

--- app/src/main/java/com/example/suh/parking/MainActivity.java
+++ app/src/main/java/com/example/suh/parking/MainActivity.java
@@ -13,19 +13,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(1500);                     //대충 만들긴했는데 여기서 로딩화면 시간 조절
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
